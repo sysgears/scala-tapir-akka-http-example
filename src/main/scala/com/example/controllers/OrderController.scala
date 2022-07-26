5eb3f65cbb34847b4fc7882de6ef33030e1af239
Oleh Vysotskyj <ovysockij2@gmail.com>
2022-07-26 16:20:51 +0300

    added quantity and product id validation

--- src/main/scala/com/example/controllers/OrderController.scala
+++ src/main/scala/com/example/controllers/OrderController.scala
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 import akka.http.scaladsl.server.{Directives, Route}
 import com.example.auth.TapirSecurity
 import com.example.dao.{OrderDao, OrderProductDao}
-import com.example.models.{Order, OrderProduct, Roles}
+import com.example.models.{AuthError, Order, OrderProduct, Roles}
 import com.example.models.forms.{CreateOrderForm, OrderProductForm}
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
@@ -14,7 +14,7 @@ import sttp.model.StatusCode
 import sttp.tapir.server.akkahttp.AkkaHttpServerInterpreter
 import sttp.tapir.statusCode
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class OrderController(tapirSecurity: TapirSecurity,
                       orderDao: OrderDao,
@@ -28,15 +28,20 @@ class OrderController(tapirSecurity: TapirSecurity,
         .example(CreateOrderForm(List(OrderProductForm(1, 5)), "Some delivery comment")))
     .out(statusCode(StatusCode.Created).description("Returns Created when order is created"))
     .serverLogic { user => newOrder =>
-      val order = Order(0, user.id, LocalDateTime.now(), "NEW", LocalDateTime.now(), newOrder.comment)
-      val products = newOrder.products.map(product => OrderProduct(0, 0, product.productId, product.quantity))
-      for {
-        orderId <- orderDao.insert(order)
-        updatedProducts = products.map(_.copy(orderId = orderId))
-        insertResult <- orderProductDao.insertBatch(updatedProducts)
-      } yield {
-        Right()
+      if (newOrder.products.forall(product => product.quantity > 0 && product.productId > 0)) {
+        val order = Order(0, user.id, LocalDateTime.now(), "NEW", LocalDateTime.now(), newOrder.comment)
+        val products = newOrder.products.map(product => OrderProduct(0, 0, product.productId, product.quantity))
+        for {
+          orderId <- orderDao.insert(order)
+          updatedProducts = products.map(_.copy(orderId = orderId))
+          insertResult <- orderProductDao.insertBatch(updatedProducts)
+        } yield {
+          Right()
+        }
+      } else {
+        Future.successful(Left(StatusCode.BadRequest, AuthError("Some order record contains invalid value!")))
       }
+
     })
 
   val orderRoutes = Directives.concat(createOrderEndpoint)
