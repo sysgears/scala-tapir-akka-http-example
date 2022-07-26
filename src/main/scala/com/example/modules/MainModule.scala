1b38fa9b21f2ff520c4533cfc03231173f6b7098
Oleh Vysotskyj <ovysockij2@gmail.com>
2022-07-26 15:27:58 +0300

    added new daos to module

--- src/main/scala/com/example/modules/MainModule.scala
+++ src/main/scala/com/example/modules/MainModule.scala
@@ -3,7 +3,7 @@ package com.example.modules
 import akka.actor.ActorSystem
 import com.example.auth.{JwtService, TapirAuthentication, TapirSecurity}
 import com.example.controllers.AuthController
-import com.example.dao.UserDao
+import com.example.dao._
 import com.softwaremill.macwire._
 import com.typesafe.config.{Config, ConfigFactory}
 import io.getquill.{PostgresJdbcContext, SnakeCase}
@@ -15,10 +15,13 @@ trait MainModule {
 
   lazy val config: Config = ConfigFactory.load()
   lazy val ctx = new PostgresJdbcContext(SnakeCase, "db.default")
-  lazy val userDao = wire[UserDao]
-  lazy val jwtService = wire[JwtService]
-  lazy val authentication = wire[TapirAuthentication]
-  lazy val tapirSecurity = wire[TapirSecurity]
-  lazy val authController = wire[AuthController]
+  lazy val userDao          = wire[UserDao]
+  lazy val orderDao         = wire[OrderDao]
+  lazy val productDao       = wire[ProductDao]
+  lazy val orderProductDao  = wire[OrderProductDao]
+  lazy val jwtService       = wire[JwtService]
+  lazy val authentication   = wire[TapirAuthentication]
+  lazy val tapirSecurity    = wire[TapirSecurity]
+  lazy val authController   = wire[AuthController]
 
 }
