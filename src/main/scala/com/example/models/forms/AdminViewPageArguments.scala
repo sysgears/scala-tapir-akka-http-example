e8a2f976e75c2a83509d2f8f9e073d08b9b4e879
Oleh Vysotskyj <ovysockij2@gmail.com>
2022-08-08 13:09:00 +0300

    removed sorting arguments

--- src/main/scala/com/example/models/forms/AdminViewPageArguments.scala
+++ src/main/scala/com/example/models/forms/AdminViewPageArguments.scala
@@ -6,9 +6,5 @@ case class AdminViewPageArguments(
   @query
   page: Int = 1,
   @query
-  pageSize: Int = 10,
-  @query
-  sortBy: Option[String],
-  @query
-  order: String = "asc"
+  pageSize: Int = 10
 )
