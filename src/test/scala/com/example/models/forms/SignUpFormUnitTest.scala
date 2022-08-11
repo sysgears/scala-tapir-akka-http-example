a98ea3ed688a626ba863661fc7eb8471e17ced4e
Oleh Vysotskyj <ovysockij2@gmail.com>
2022-08-11 15:29:12 +0300

    added unit tests for sign up form validation

---
+++ src/test/scala/com/example/models/forms/SignUpFormUnitTest.scala
@@ -0,0 +1,26 @@
+package com.example.models.forms
+
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SignUpFormUnitTest extends AsyncFlatSpec with Matchers {
+
+  val form: SignUpForm = SignUpForm("test name", "+77777777777", "test@example.com", "49050", "Dnipro", "test address, 46", "pass456", "pass456")
+
+  it should "validate correct form correctly" in {
+    form.isValid.isRight shouldBe true
+  }
+
+  it should "validate incorrect email correctly" in {
+    form.copy(email = "test example.com").isValid.isLeft shouldBe true
+  }
+
+  it should "validate too short password correctly" in {
+    form.copy(password = "pass", repeatPassword = "pass").isValid.isLeft shouldBe true
+  }
+
+  it should "validate not matching passwords correctly" in {
+    form.copy(repeatPassword = "pass4567").isValid.isLeft shouldBe true
+  }
+
+}
