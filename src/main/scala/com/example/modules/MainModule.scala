7b88b300d00593ead90aa8b4c645c9f0a458a46a
olehv <oleh.vysotskyi@sysgears.com>
2025-12-30 12:32:56 +0200

    adds type annotation for layers

--- src/main/scala/com/example/modules/MainModule.scala
+++ src/main/scala/com/example/modules/MainModule.scala
@@ -1,10 +1,15 @@
 package com.example.modules
 
 import akka.actor.ActorSystem
+import com.example.auth.Jwt.JwtService
 import com.example.auth.TapirAuthentication.TapirAuth
 import com.example.auth.{Jwt, TapirAuthentication, TapirSecurity}
 import com.example.controllers.admin._
 import com.example.controllers._
+import com.example.dao.OrderDao.OrderRepository
+import com.example.dao.OrderProductDao.OrderProductRepository
+import com.example.dao.ProductDao.ProductRepository
+import com.example.dao.UserDao.UserRepository
 import com.example.dao._
 import com.example.errors.ErrorHandler
 import com.example.services.AuthService.Authentication
@@ -36,27 +41,27 @@ trait MainModule {
   lazy val config = ZLayer.succeed(ConfigFactory.load())
   lazy val postgres: ZLayer[DataSource, Nothing, Quill.Postgres[SnakeCase.type]] = Quill.Postgres.fromNamingStrategy(SnakeCase)
   lazy val dataSource: ZLayer[Any, Throwable, DataSource] = Quill.DataSource.fromPrefix("db.default")
-  lazy val userDao = UserDao.live
-  lazy val orderDao = OrderDao.live
-  lazy val productDao = ProductDao.live
-  lazy val orderProductDao = OrderProductDao.live
-  lazy val jwtService = Jwt.live
-  lazy val tapirAuth = TapirAuthentication.live
-  lazy val authService = AuthService.live
-  lazy val orderService = OrderService.live
-  lazy val productService = ProductService.live
-  lazy val adminProductService = AdminProductService.live
-  lazy val adminOrderService = AdminOrderService.live
+  lazy val userDao: ZLayer[Quill.Postgres[SnakeCase], Nothing, UserRepository] = UserDao.live
+  lazy val orderDao: ZLayer[Quill.Postgres[SnakeCase], Nothing, OrderRepository] = OrderDao.live
+  lazy val productDao: ZLayer[Quill.Postgres[SnakeCase], Nothing, ProductRepository] = ProductDao.live
+  lazy val orderProductDao: ZLayer[Quill.Postgres[SnakeCase], Nothing, OrderProductRepository] = OrderProductDao.live
+  lazy val jwtService: ZLayer[Config, Nothing, JwtService] = Jwt.live
+  lazy val tapirAuth: ZLayer[UserRepository with JwtService, Nothing, TapirAuth] = TapirAuthentication.live
+  lazy val authService: ZLayer[JwtService with UserRepository, Nothing, Authentication] = AuthService.live
+  lazy val orderService: ZLayer[OrderProductRepository with ProductRepository with OrderRepository, Nothing, OrdersService] = OrderService.live
+  lazy val productService: ZLayer[ProductRepository, Nothing, ProductService] = ProductService.live
+  lazy val adminProductService: ZLayer[ProductRepository, Nothing, AdminProducts] = AdminProductService.live
+  lazy val adminOrderService: ZLayer[OrderProductRepository with UserRepository with ProductRepository with OrderRepository, Nothing, AdminOrders] = AdminOrderService.live
 
   // ULayer creation. The reason - to compose layers into single one for convenient transfer to controllers
   // Layer wiring is done automatically, you just need to provide all required zlayers.
   // orDie forces to throw an error if anything happens in the app's start
-  lazy val authenticationLayer = ZLayer.make[TapirAuth](tapirAuth, userDao, dataSource, postgres, jwtService, config).orDie
-  lazy val authServiceLayer = ZLayer.make[Authentication](authService, userDao, dataSource, postgres, jwtService, config).orDie
-  lazy val orderServiceLayer = ZLayer.make[OrdersService](orderService, orderDao, orderProductDao, productDao, dataSource, postgres).orDie
-  lazy val productServiceLayer = ZLayer.make[ProductService](productService, productDao, dataSource, postgres).orDie
-  lazy val adminProductServiceLayer = ZLayer.make[AdminProducts](adminProductService, productDao, dataSource, postgres).orDie
-  lazy val adminOrderServiceLayer = ZLayer.make[AdminOrders](adminOrderService, orderDao, userDao, orderProductDao, productDao, dataSource, postgres).orDie
+  lazy val authenticationLayer: ZLayer[Any, Nothing, TapirAuth] = ZLayer.make[TapirAuth](tapirAuth, userDao, dataSource, postgres, jwtService, config).orDie
+  lazy val authServiceLayer: ZLayer[Any, Nothing, Authentication] = ZLayer.make[Authentication](authService, userDao, dataSource, postgres, jwtService, config).orDie
+  lazy val orderServiceLayer: ZLayer[Any, Nothing, OrdersService] = ZLayer.make[OrdersService](orderService, orderDao, orderProductDao, productDao, dataSource, postgres).orDie
+  lazy val productServiceLayer: ZLayer[Any, Nothing, ProductService] = ZLayer.make[ProductService](productService, productDao, dataSource, postgres).orDie
+  lazy val adminProductServiceLayer: ZLayer[Any, Nothing, AdminProducts] = ZLayer.make[AdminProducts](adminProductService, productDao, dataSource, postgres).orDie
+  lazy val adminOrderServiceLayer: ZLayer[Any, Nothing, AdminOrders] = ZLayer.make[AdminOrders](adminOrderService, orderDao, userDao, orderProductDao, productDao, dataSource, postgres).orDie
 
   lazy val authController = new AuthController(authServiceLayer)
   lazy val tapirSecurity = new TapirSecurity(authenticationLayer)
