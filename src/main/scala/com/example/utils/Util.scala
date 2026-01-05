8181899419144e33974efb4523ab2e9e64078a36
olehv <oleh.vysotskyi@sysgears.com>
2026-01-05 18:33:54 +0200

    removes redundant util function

--- src/main/scala/com/example/utils/Util.scala
+++ src/main/scala/com/example/utils/Util.scala
@@ -5,22 +5,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object Util {
 
-  /** Function, which converts either with future to future with either.
-   *
-    * @param e target either
-    * @param ec execution context for future convertion
-    * @tparam A error type
-    * @tparam B success type
-    * @return future with either.
-    */
-  def foldEitherOfFuture[A, B](
-      e: Either[A, Future[B]]
-  )(implicit ec: ExecutionContext): Future[Either[A, B]] =
-    e match {
-      case Left(s)  => Future.successful(Left(s))
-      case Right(f) => f.map(Right(_))
-    }
-
   /** Email regex. Taken from Play forms. */
   private val emailRegex =
     """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
